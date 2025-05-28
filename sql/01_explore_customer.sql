19e9bcdcfc6e01c96361f9e164e91980f18897c0
naufaldahafizh <naufaldzakihafizhan@gmail.com>
2025-05-28 10:38:04 +0700

    Add churn overview

--- sql/01_explore_customer.sql
+++ sql/01_explore_customer.sql
@@ -1,4 +1,17 @@
--- Rata-rata tenure dan monthly charges per contract type
-SELECT contract, AVG(tenure) as avg_tenure, AVG(MonthlyCharges) as avg_monthly
-FROM telco
-GROUP BY contract;
+-- 01_explore_customers.sql
+-- 1. Hitung total pelanggan dan churn rate
+SELECT
+  COUNT(*) AS total_customers,
+  SUM(CASE WHEN churn = 'Yes' THEN 1 ELSE 0 END)::FLOAT / COUNT(*) AS churn_rate
+FROM telco.churn;
+
+-- 2. Rata-rata tenure per gender
+SELECT gender, AVG(tenure) AS avg_tenure
+FROM telco.churn
+GROUP BY gender;
+
+-- 3. Distribusi payment method
+SELECT PaymentMethod, COUNT(*) AS count_customers
+FROM telco.churn
+GROUP BY PaymentMethod
+ORDER BY count_customers DESC;
